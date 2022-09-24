0d934f0ef9db48aeca0d276edf01deadb72a31cb
bsolis6878 <bsolis6878@gmail.com>
2022-09-24 12:12:01 -0500

    added html routes, links work on server

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // sets port number to listen on
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,15 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+// sets port for server to listen on
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
